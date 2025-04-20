2372c2197ecdef94f0d879bcb7b3eb3917beb2d9
nasa14119 <nico.amaya0205@gmail.com>
2025-04-20 14:47:46 -0600

    feat(ffunc): adding gradiente func for any given point

--- func.py
+++ func.py
@@ -8,3 +8,13 @@ def func(x, y):
         - 0.005 * y**2
         + 20 * np.sin(x * np.pi / 50) * np.cos(y * np.pi / 50)
     )
+
+
+def gradient_func(x, y):
+    func_x = lambda x, y: -0.01 * x + np.pi * 2 * np.pow(5.0, -1) * np.cos(
+        np.pi * x * np.pow(50.0, -1)
+    ) * np.cos(np.pi * y * np.pow(50.0, -1))
+    func_y = lambda x, y: -0.01 * y - np.pi * 2 * np.pow(5.0, -1) * np.sin(
+        np.pi * x * np.pow(50.0, -1)
+    ) * np.sin(np.pi * y * np.pow(50.0, -1))
+    return np.array([func_x(x, y), func_y(x, y)])
